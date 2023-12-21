519a04f6f677c4afb252f0ab4a37473fb2ab0e84
Zachary Williams <zackwilldev@gmail.com>
2023-12-21 09:25:51 -0500

    new spotify controller

--- app/javascript/controllers/show_controller.js
+++ app/javascript/controllers/show_controller.js
@@ -1,41 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="show"
 export default class extends Controller {
-  static targets = [ 'episode', 'timeStamp' ]
-  static values = { uri: String }
 
   connect() {
-
     this.setupTurboStreamListener();
-    this.initializeEmbed();
-  }
-
-  async initializeEmbed() {
-    try {
-      window.onSpotifyIframeApiReady = (IFrameAPI) => {
-        const element = this.episodeTarget;
-        const options = {
-          width: '300',
-          height: '200',
-          uri: this.uriValue
-        };
-        const callback = (EmbedController) => {
-          EmbedController.addListener('playback_update', e => {
-            this.timeStampTarget.value = e.data.position
-            });
-        };
-        IFrameAPI.createController(element, options, callback)
-        ;
-      };
-      
-    } catch (error) {
-      console.error('Error initializing Spotify Embed:', error);
-    }
-  }
-
-  get embedController() {
-    return this.data.get('embedController');
   }
 
   setupTurboStreamListener() {
--- app/javascript/controllers/spotify_controller.js
+++ app/javascript/controllers/spotify_controller.js
@@ -2,45 +2,38 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="spotify"
 export default class extends Controller {
+  static targets = [ 'episode', 'timeStamp' ]
+  static values = { uri: String }
+  
   connect() {
-      this.renderSpotifySDK();
-  }
-  renderSpotifySDK() {
-    // Create the main container div
-    const container = document.createElement('div');
-
-    // Set the HTML content
-    container.innerHTML = `
-      <h1>Spotify Web Playback SDK Quick Start</h1>
-      <button data-action="click->spotify#togglePlay">Toggle Play</button>
-    `;
-
-    // Create the script element for the Spotify SDK
-    const spotifyScript = document.createElement('script');
-    spotifyScript.src = 'https://sdk.scdn.co/spotify-player.js';
-
-    // Create the script element for the inline JavaScript
-    const inlineScript = document.createElement('script');
-    inlineScript.innerHTML = `
-      window.onSpotifyWebPlaybackSDKReady = () => {
-          const token = 'BQBRjjIU60MyNvdA1Dj_F2zOWi9dnUGbFjIznK6yLg2bXb3T0ieHvAklhrpkMkj62uyZM-KyC-YjsdcMzw4I-n0AN-NmgNO19FyLjBhljpdPpfJJggvVGCCkXnqPdGxSdwGUbJKS5EdDfzb5diAUvHJggaHj_A34EbDYSRYfYkwpnyTPAT4hBzvL7hITDVncTZTC';
-          const player = new Spotify.Player({
-              name: 'Web Playback SDK Quick Start Player',
-              getOAuthToken: cb => { cb(token); },
-              volume: 0.5
-          });
-
-          // ... (the rest of your Spotify SDK logic)
-      }
-    `;
-
-    // Append the created elements to the element associated with this controller
-    this.element.appendChild(container);
-    this.element.appendChild(spotifyScript);
-    this.element.appendChild(inlineScript);
+    this.initializeEmbed();
   }
 
-  togglePlay() {
-    // Handle the toggle play logic here
+  async initializeEmbed() {
+    try {
+      window.onSpotifyIframeApiReady = (IFrameAPI) => {
+        const element = this.episodeTarget;
+        const options = {
+          width: '300',
+          height: '200',
+          uri: this.uriValue
+        };
+        const callback = (EmbedController) => {
+          EmbedController.addListener('playback_update', e => {
+            this.timeStampTarget.value = e.data.position
+            });
+        };
+        IFrameAPI.createController(element, options, callback)
+        ;
+      };
+      
+    } catch (error) {
+      console.error('Error initializing Spotify Embed:', error);
+    }
   }
+
+  get embedController() {
+    return this.data.get('embedController');
+  }
+
 }
