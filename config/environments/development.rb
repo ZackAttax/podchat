c42b156d4901236c93e0455e1adb8110f4533317
Zachary Williams <zackwilldev@gmail.com>
2024-01-18 13:18:48 -0500

    remove '/'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
     port: 587,
-    domain: 'localhost:3000/', # Replace with your actual domain
+    domain: 'localhost:3000', # Replace with your actual domain
     user_name: 'apikey',
     password: ENV['SENDGRID_API_KEY'] || "",
     authentication: :plain,
