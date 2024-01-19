90b699c2174ac49e6d5bbbd9d571e0ca45a7328d
Zachary Williams <zackwilldev@gmail.com>
2024-01-19 15:27:05 -0500

    use add_search_to_BC before_action in episode controller

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,9 +1,9 @@
 class EpisodesController < ApplicationController
   before_action :authenticate_user!
+  before_action :add_search_to_breadcrumb
 
   def show
     @episode = SpotifyApi.find_episode(params[:id])
-    add_breadcrumb "Search", podcasts_search_path
     add_breadcrumb @episode.show.name, podcasts_show_url(@episode.show.id)
     add_breadcrumb @episode.name, episode_url(@episode.id)
     @comments = Comment.by_episode_in_order_by_timestamp(@episode.id).includes(:user, :replies)
